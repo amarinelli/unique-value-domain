c66a3302739ff0e626c5104860af006736be0467
Adam <amarinelli@esri.ca>
2016-03-07 17:17:16 -0500

    Module setup

--- UniqueValues.pyt
+++ UniqueValues.pyt
@@ -1,4 +1,5 @@
 import arcpy
+import unique
 
 
 class Toolbox(object):
---
+++ unique/unique.py
@@ -0,0 +1,8 @@
+#! python2.7
+
+"""
+Module for gathering unique values from an input.
+Focused on ArcGIS specific inputs such as field values of a feature class.
+"""
+
+import arcpy
